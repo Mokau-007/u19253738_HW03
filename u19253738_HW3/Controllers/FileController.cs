f636ffc4d26217747ad49d527908a78974ce3d2c
Paballo <paballomasemola@gmail.com>
2022-06-07 17:29:01 +0200

    Added file controller

---
+++ u19253738_HW3/Controllers/FileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u19253738_HW3.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
