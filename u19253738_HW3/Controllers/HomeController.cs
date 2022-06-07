6b89f1384712419a87f89846ac494f3ca63057db
Paballo <paballomasemola@gmail.com>
2022-06-07 17:00:18 +0200

    Added code in HomeController Index ActionResult, to upload files to required folders,

--- u19253738_HW3/Controllers/HomeController.cs
+++ u19253738_HW3/Controllers/HomeController.cs
@@ -3,20 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace u19253738_HW3.Controllers
 {
     public class HomeController : Controller
     {
+        
+        [HttpGet]
         public ActionResult Index()
         {
+          
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string FileType, HttpPostedFileBase files)
+        {
+            
+            if (FileType == "Document")
+            {
+                if(files != null & files.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(files.FileName);
+
+                    // store the file inside ~/App_Data/uploads folder
+
+                    var path = Path.Combine(Server.MapPath("~/App_Data/Media/Documents"), fileName);
+
+                    // The chosen default path for saving
+
+                    files.SaveAs(path);
+                }
+            }
+            if (FileType == "Document")
+            {
+                if (files != null & files.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(files.FileName);
+
+                    // store the file inside ~/App_Data/uploads folder
+
+                    var path = Path.Combine(Server.MapPath("~/App_Data/Media/Documents"), fileName);
+
+                    // The chosen default path for saving
+
+                    files.SaveAs(path);
+                }
+            }
+            if (FileType == "Image")
+            {
+                if (files != null & files.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(files.FileName);
+
+                    // store the file inside ~/App_Data/uploads folder
+
+                    var path = Path.Combine(Server.MapPath("~/App_Data/Media/Images"), fileName);
+
+                    // The chosen default path for saving
+
+                    files.SaveAs(path);
+                    
+                }
+            }
+            if (FileType == "Videos")
+            {
+                if (files != null & files.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(files.FileName);
+
+                    // store the file inside ~/App_Data/uploads folder
+
+                    var path = Path.Combine(Server.MapPath("~/App_Data/Media/Videos"), fileName);
+
+                    // The chosen default path for saving
+
+                    files.SaveAs(path);
+                }
+            }
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+           
             return View();
         }
 
